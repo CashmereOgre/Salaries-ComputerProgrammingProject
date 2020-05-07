fdf0802072fa0064af44a9676587b4c38d703e7c
CashmereOgre <43883388+CashmereOgre@users.noreply.github.com>
2020-05-07 19:19:03 +0200

    Functions for calculating salary and contributions

---
+++ Project/Salaries/Salaries/Worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salaries
+{
+    class Worker
+    {
+        private string Name;
+        private string Surname;
+        private char Gender;
+        private DateTime Birthdate;
+        private long Pesel;
+        public int years_of_work;
+    }
+}
